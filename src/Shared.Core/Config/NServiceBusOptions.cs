5fce025ddcd4902b9def6ed9dc2edb3b25fe57f1
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2020-05-10 00:36:53 +0100

    Removed unnecessary code, configure swagger for api, added appsettings and environment file for docker-compose

---
+++ src/Shared.Core/Config/NServiceBusOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Core.Config
+{
+    public class NServiceBusOptions
+    {
+        public string SqsServiceUrlOverride { get; set; }
+
+        public string S3ServiceUrlOverride { get; set; }
+    }
+}
