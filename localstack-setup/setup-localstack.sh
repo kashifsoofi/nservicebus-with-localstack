4b0c05296616e282b51dc9b3ce0db9b27f2d8b02
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-02-25 11:32:49 +0000

    Update to dotnet 6.0, fix create bucket command

--- localstack-setup/setup-localstack.sh
+++ localstack-setup/setup-localstack.sh
@@ -12,5 +12,5 @@ aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --
 aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --queue-name Samples-FullDuplex-Client-1
 aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --queue-name Samples-FullDuplex-Server
 aws --region eu-west-1 --endpoint-url=http://localstack:4566 sqs create-queue --queue-name error
-aws --region eu-west-1 --endpoint-url=http://localstack:4566 s3api create-bucket --bucket bucketname
-aws --endpoint-url=http://localstack:4566 s3api put-bucket-acl --bucket bucketname --acl public-read
+aws --endpoint-url=http://localstack:4566 s3api create-bucket --bucket bucketname --region eu-west-1 --create-bucket-configuration LocationConstraint=eu-west-1
+aws --endpoint-url=http://localstack:4566 s3api put-bucket-acl --bucket bucketname --acl public-read --region eu-west-1
